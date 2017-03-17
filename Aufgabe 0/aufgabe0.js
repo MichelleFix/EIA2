8675b7c833f2330ad55a3361e0c9d913f12ac439
Michelle <Michelle@141.28.8.55>
2017-03-17 18:29:18 +0100

    typescript und htmlupload

---
+++ Aufgabe 0/aufgabe0.js
@@ -0,0 +1,15 @@
+/*
+Aufgabe: Aufgabe 0
+Name: Michelle Fix
+Matrikel: 254671
+Datum: 17.03.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    var name;
+    name = prompt("Wie ist Ihr Name?");
+    document.getElementById("begrue�ung").innerHTML += name;
+});
+//# sourceMappingURL=aufgabe0.js.map
