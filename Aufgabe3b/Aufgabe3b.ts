c90897f53df9a68fdfda9378d5a1cc13cfebd967
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2017-04-22 13:08:00 +0200

    Erforderliche Korrekturen mit <-- gekennzeichnet

--- Aufgabe3b/Aufgabe3b.ts
+++ Aufgabe3b/Aufgabe3b.ts
@@ -7,29 +7,29 @@
 //Code selbst geschrieben habe. Er wurde
 //nicht kopiert und auch nicht diktiert.
 
-//Frage: wie kann ich die erstellten divs ansprechen??? Zeile 51
+//Frage: wie kann ich die erstellten divs ansprechen??? Zeile 51   <-- ist der Kommentar noch relevant?
 //         wie kann es dir variable 'div' auch weiter unten noch erkennen? zeile 61
 
 namespace maumau {
-    var cards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik As"];
-    var handCards: string[] = []; //leeres Array für die Handkarten
-    var discard: string[] = []; //leeres Array für den Ablagestapel
+    var cards <-- Typdeklaration? = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo KÃ¶nig", "Karo As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz KÃ¶nig", "Kreuz As", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz KÃ¶nig", "Herz As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik KÃ¶nig", "Pik As"];
+    var handCards: string[] = []; //leeres Array fÃ¼r die Handkarten
+    var discard: string[] = []; //leeres Array fÃ¼r den Ablagestapel
 
 
     function moveToHand(): void {
-        if (handCards.length < 5) {         //Es dürfen nur max.  karten auf die hand
+        if (handCards.length < 5) {         //Es dÃ¼rfen nur max.  karten auf die hand
             let min: number = 0;
             let max: number = cards.length - 1;
-            var value: number = Math.round(Math.random() * (max - min) + min);
+            var value: number = Math.round(Math.random() * (max - min) + min); <-- hier auch besser mit let arbeiten
             
             let pulledCard: string = cards[value];
             console.log(pulledCard);      //die gezogene Karte 
             cards.splice(value, 1);                     // EIN Element, das an der Stelle "value" kommt, wird aus dem array entfernt
-            handCards.push(pulledCard);            // Das entfernte element wird in das array handcards an der stelle 0 eingefügt. also ganz noch vorne
+            handCards.push(pulledCard);            // Das entfernte element wird in das array handcards an der stelle 0 eingefÃ¼gt. also ganz noch vorne
             document.getElementById("drawpile").textContent = "Nachziehstapel " + "\r\n" + "Karten: " + cards.length;
             // KURZSCHREIBWEISE:           handCards.push(cards.splice(value, 1)[0]);
 
-            //danach soll ein Div für die neue Karte ertsellt werden.
+            //danach soll ein Div fÃ¼r die neue Karte ertsellt werden.
             var div: HTMLDivElement = document.createElement("div");
             document.getElementById("handcard").appendChild(div);
             div.textContent = pulledCard;           //darauf soll geschrieben werden, um welche karte es sich handelt
@@ -54,8 +54,8 @@ namespace maumau {
         let div: HTMLDivElement = <HTMLDivElement> _event.target;
         console.log(div);
         for (var i = 0; i < handCards.length; i++) {
-            handCards.splice(i, 1);
-            discard.push(handCards[i]);
+            handCards.splice(i, 1); <-- der Splice darf erst nach den nÃ¤chsten beiden Anweisungen kommen,
+            discard.push(handCards[i]); <-- sonst gibt es die Karte in handCards hier schon nicht mehr, daher der Fehler...
             currentCard = handCards[i];
             break;            
             
@@ -64,17 +64,17 @@ namespace maumau {
         div.remove();
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() { <-- keine anonymen Funktionen!
+        <-- die folgenden drei Zeilen sind Ã¼berflÃ¼ssig und irritierend, die entsprechenden Variablen sind doch schon im namespace angelegt!
+        var cards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo KÃ¶nig", "Karo As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz KÃ¶nig", "Kreuz As", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz KÃ¶nig", "Herz As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik KÃ¶nig", "Pik As"];
+        var handCards: string[] = []; //leeres Array fÃ¼r die Handkarten
+        var discard: string[] = []; //leeres Array fÃ¼r den Ablagestapel
 
-        var cards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik As"];
-        var handCards: string[] = []; //leeres Array für die Handkarten
-        var discard: string[] = []; //leeres Array für den Ablagestapel
 
-
-        // 1. Bei jedem Klick auf den nachziehstapel muss eine random karte auf die hand wandern. Die Random karte muss aus dem Nachziehstapel gelöscht werden
+        // 1. Bei jedem Klick auf den nachziehstapel muss eine random karte auf die hand wandern. Die Random karte muss aus dem Nachziehstapel gelÃ¶scht werden
 
         document.getElementById("drawpile").addEventListener("click", moveToHand);
-        console.log("click draw");
+        console.log("click draw"); <-- was bring das?
 
         //Von der Hand auf den Ablagestapel
         document.getElementById("handcard").addEventListener("click", moveToDiscard);
