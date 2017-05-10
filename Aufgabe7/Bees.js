c1da2b061c3181bdd0f00993d0186daabf0b682b
Michelle <Michelle@Lenovo-B50-70.hfu-open.fu.hs-furtwangen.de>
2017-05-10 16:57:30 +0200

    Steckbrief

---
+++ Aufgabe7/Bees.js
@@ -0,0 +1,15 @@
+var L7_Classes;
+(function (L7_Classes) {
+    var Bee = (function () {
+        function Bee(_x, _y) {
+            this.setRandomStyle();
+            //this.setRandomPosition();
+            this.x = _x;
+            this.y = _y;
+        }
+        Bee.prototype.setRandomStyle = function () { };
+        return Bee;
+    }());
+    L7_Classes.Bee = Bee;
+})(L7_Classes || (L7_Classes = {}));
+//# sourceMappingURL=Bees.js.map
---
+++ Aufgabe7/Bees.ts
@@ -0,0 +1,27 @@
+namespace L7_Classes {
+    export class Bee {
+        x: number;
+        y: number;
+        color: string;
+        wingSize: number;
+        
+         constructor(_x: number, _y: number) {
+            this.setRandomStyle();
+            //this.setRandomPosition();
+            this.x = _x;
+            this.y = _y;
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        setRandomStyle():void{}
+
+    }
+}
