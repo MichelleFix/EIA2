9a779cf6b6a91e396c28d9dafd1d9e9937f11160
Michelle <Michelle@Lenovo-B50-70.Speedport_W_724V_Typ_A_05011603_05_020>
2017-06-25 17:37:18 +0200

    Steckbrief

--- Aufgabe10/SendData.js
+++ Aufgabe10/SendData.js
@@ -20,8 +20,8 @@ var SendData;
     }
     function sendRequest(_color) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8100?color=" + _color, true);
-        //xhr.open("GET", "https://eia2-servertest.herokuapp.com?color=" + _color, true);
+        //xhr.open("GET", "http://localhost:8100?color=" + _color, true);
+        xhr.open("GET", "https://senddata.com?color=" + _color, true);
         xhr.addEventListener("readystatechange", handleStateChange);
         xhr.send();
     }
